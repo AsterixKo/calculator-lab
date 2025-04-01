5279ae97624450f5cddc4cc96eb8ec2efbdbdfeb
Pacheca <mpacheca7@gmail.com>
2025-04-01 15:29:24 +0200

    Sub miguel

---
+++ src/main/java/org/jmc/mydomain/Sub.java
@@ -0,0 +1,21 @@
+package org.jmc.mydomain;
+
+public class Sub {
+        public static double restdouble(double num1, double num2) {
+            double decimal = num1 - num2;
+
+            return decimal;
+        }
+
+        public static int restint (int num1, int num2) {
+            int entero = num1 - num2;
+
+            return entero;
+        }
+
+        public static double restriple (double num1, double num2, double num3) {
+            double triple = num1 - num2 - num3;
+
+            return triple;
+        }
+    }

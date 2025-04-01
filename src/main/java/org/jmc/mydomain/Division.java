17c1856a9ce76149b34377a7ee05156b4dba1ecc
JesusCiber <fjesuscl@gmail.com>
2025-04-01 15:44:02 +0200

    Implementación de la clase Division

---
+++ src/main/java/org/jmc/mydomain/Division.java
@@ -0,0 +1,16 @@
+package org.jmc.mydomain;
+
+public class Division {
+    public static double NumerosReales(double a, double b){
+        double division = a/b;
+        return division;
+    }
+    public static int NumerosEnteros(int a, int b){
+        int division = a/b;
+        return division;
+    }
+    public static double NumeroReal(double a){
+        double inverso = 1 / a;
+        return inverso;
+    }
+}
